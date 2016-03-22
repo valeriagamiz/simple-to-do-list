ff9024ac05a7ef9e4b6f2413f5911118caf71ee7
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-03-22 01:05:17 -0400

    s

--- js/main.js
+++ js/main.js
@@ -1,14 +1,2 @@
 var $input = $('#to-do-input');
-var $list = $('.list');
-
-$('form').on('submit', function (e) {
-  var $li = $('<li>');
-
-  e.preventDefault();
-  $li.html($input.val());
-  $list.append($li);
-});
-
-$('ul').on('click', 'li', function (e) {
-  $(this).toggleClass('strike');
-});
+var $list = $('.list');s
